444877d01d7fb3dde7dfa9d08ad68f67901c9d85
Mdd 0x <adityamahaputra18@gmail.com>
2023-02-14 20:36:28 +0700

    jnj

--- client/assets/js/connect.js
+++ client/assets/js/connect.js
@@ -1,21 +1,12 @@
-if (window.ethereum) {
-    handleEthereum();
-  } else {
-    window.addEventListener('ethereum#initialized', handleEthereum, {
-      once: true,
-    });
-  
-    // If the event is not dispatched by the end of the timeout,
-    // the user probably doesn't have MetaMask installed.
-    setTimeout(handleEthereum, 3000); // 3 seconds
-  }
-  
-  function handleEthereum() {
-    const { ethereum } = window;
-    if (ethereum && ethereum.isMetaMask) {
-      console.log('Ethereum successfully detected!');
-      // Access the decentralized web!
-    } else {
-      console.log('Please install MetaMask!');
-    }
-  }
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
